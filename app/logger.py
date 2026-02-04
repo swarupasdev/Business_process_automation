39fc1f972b3a7579499cabe36fcce1da906a31f1
Swarup143 <santiswarupnayak1@gmail.com>
2026-02-04 23:45:47 +0530

    logger file fix

--- app/logger.py
+++ app/logger.py
@@ -1,9 +1,13 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+from app.paths import BASE_DIR
 
 def setup_logger():
-    os.makedirs("logs", exist_ok=True)
+    logs_dir = os.path.join(BASE_DIR, "logs")
+    os.makedirs(logs_dir, exist_ok=True)
+
+    log_file = os.path.join(logs_dir, "app.log")
 
     logger = logging.getLogger("automation")
     logger.setLevel(logging.INFO)
@@ -12,7 +16,7 @@ def setup_logger():
         return logger
 
     handler = RotatingFileHandler(
-        "logs/app.log",
+        log_file,
         maxBytes=1_000_000,
         backupCount=5
     )
